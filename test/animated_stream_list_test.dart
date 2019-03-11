a85ae7a2e4ffc854cf5b4ad489b04d0d9b190c73
Otsoa <otsoaunloco@protonmail.ch>
2019-03-11 21:54:25 +0100

    write more test cases

--- test/animated_stream_list_test.dart
+++ test/animated_stream_list_test.dart
@@ -7,7 +7,7 @@ import 'package:quiver/collection.dart';
 import 'package:test/test.dart';
 
 main() {
-  test("check if diff works", () async {
+  test("check if handles bigger list", () async {
     final list1 = [1, 2, 3, 4];
     final list2 = [2, 1, 3, 6, 7, 8];
     final controller = ListController<int>(
@@ -19,13 +19,117 @@ main() {
     );
 
     final diffs = await DiffUtil<int>().calculateDiff(controller.items, list2);
-    print (diffs.join(", "));
     DiffApplier(controller).applyDiffs(diffs);
 
-    print("list1: ${controller.items.join(", ")}");
-    print("list2: ${list2.join(", ")}");
     final areEqual = listsEqual(controller.items, list2);
     expect(areEqual, true);
+  });
+
+  test("check if handles smaller list", () async {
+    final list1 = [2, 1, 3, 6, 7, 8];
+    final list2 = [1, 2, 3, 4];
+    final controller = ListController<int>(
+      items: list1,
+      key: MockKey(),
+      itemRemovedBuilder: (int element, int index, BuildContext context,
+              Animation<double> animation) =>
+          null,
+    );
+
+    final diffs = await DiffUtil<int>().calculateDiff(controller.items, list2);
+    DiffApplier(controller).applyDiffs(diffs);
+
+    final areEqual = listsEqual(controller.items, list2);
+    expect(areEqual, true);
+  });
+
+  test("check if handles equal sized lists", () async {
+    final list1 = [2, 1, 3, 6, 7, 8];
+    final list2 = [1, 2, 3, 4, 0, 8];
+    final controller = ListController<int>(
+      items: list1,
+      key: MockKey(),
+      itemRemovedBuilder: (int element, int index, BuildContext context,
+                           Animation<double> animation) =>
+      null,
+    );
+
+    final diffs = await DiffUtil<int>().calculateDiff(controller.items, list2);
+    DiffApplier(controller).applyDiffs(diffs);
+
+    final areEqual = listsEqual(controller.items, list2);
+    expect(areEqual, true);
+  });
+
+  test("check if handles equal lists", () async {
+    final list1 = [2, 1, 3, 6, 7, 8];
+    final list2 = [2, 1, 3, 6, 7, 8];
+    final controller = ListController<int>(
+      items: list1,
+      key: MockKey(),
+      itemRemovedBuilder: (int element, int index, BuildContext context,
+                           Animation<double> animation) =>
+      null,
+    );
+
+    final diffs = await DiffUtil<int>().calculateDiff(controller.items, list2);
+    DiffApplier(controller).applyDiffs(diffs);
+
+    final areEqual = listsEqual(controller.items, list2);
+    expect(areEqual, true);
+  });
+
+  test("check if throws when second list is null", () async {
+    final list1 = [1];
+    final list2 = null;
+    final controller = ListController<int>(
+      items: list1,
+      key: MockKey(),
+      itemRemovedBuilder: (int element, int index, BuildContext context,
+              Animation<double> animation) =>
+          null,
+    );
+
+    final diffs = DiffUtil<int>().calculateDiff(controller.items, list2);
+    expect(() async => await diffs, throwsException);
+  });
+
+  group("check if handles one list being empty", () {
+    test("first list empty", () async {
+      final list1 = <int>[];
+      final list2 = [1,2,3];
+      final controller = ListController<int>(
+        items: list1,
+        key: MockKey(),
+        itemRemovedBuilder: (int element, int index, BuildContext context,
+                             Animation<double> animation) =>
+        null,
+      );
+
+      final diffs = await DiffUtil<int>().calculateDiff(controller.items, list2);
+      DiffApplier(controller).applyDiffs(diffs);
+
+      final areEqual = listsEqual(controller.items, list2);
+      expect(areEqual, true);
+    });
+
+    test("second list empty", () async {
+      final list1 = [1,2,3];
+      final list2 = <int>[];
+      final controller = ListController<int>(
+        items: list1,
+        key: MockKey(),
+        itemRemovedBuilder: (int element, int index, BuildContext context,
+                             Animation<double> animation) =>
+        null,
+      );
+
+      final diffs = await DiffUtil<int>().calculateDiff(controller.items, list2);
+      DiffApplier(controller).applyDiffs(diffs);
+
+      final areEqual = listsEqual(controller.items, list2);
+      expect(areEqual, true);
+    });
   });
 }
 
